8ac7f63d811f2fa0843f49784be5b5a67cd59aee
Lillian Yang <lyang@lcs.on.ca>
2022-10-27 11:15:46 -0400

    Initial Commit

---
+++ ColourSelector/ColourSelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColourSelectorApp.swift
+//  ColourSelector
+//
+//  Created by Lillian Yang on 2022-10-27.
+//
+
+import SwiftUI
+
+@main
+struct ColourSelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
